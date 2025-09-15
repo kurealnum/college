aea28459e78a3617a1935bc5944417ca3e08edd2
kurealnum <oscar.gaske.cs@gmail.com>
2025-09-15 08:41:37 -0400

    lab 2 in lab 1

--- 2025-2026/CPSC-240/lab1/src/Main.java
+++ 2025-2026/CPSC-240/lab1/src/Main.java
@@ -1,29 +1,47 @@
+/**
+ * The "top" class in this program
+ *
+ * @author Oscar Gaske
+ */
 public class Main {
+    /**
+     * The main function
+     *
+     * @param args optional command line arguments
+     */
     public static void main(String[] args) {
         Room jamesFarmer140 = new Room();
 
         jamesFarmer140.setRoomNum(140);
         jamesFarmer140.setBuildingName("James Farmer");
-        jamesFarmer140.setSeatingCap(30);
-
-        Room univerityCenter200 = new Room(30, "University Center", 200);
-
-        if (univerityCenter200.getRoomNum() != 200) {
-            System.out.println("Something went wrong with getRoomNum");
-        } else {
-            System.out.println("getRoomNum works!");
+        try {
+            jamesFarmer140.setSeatingCap(30);
+        } catch (Exception e) {
+            System.out.println("Seating cap cannot be negative. Error: " + e);
         }
 
-        if (univerityCenter200.getSeatingCap() != 30) {
-            System.out.println("Something went wrong with getRoomNum");
-        } else {
-            System.out.println("getSeatingCap works!");
-        }
+        try {
+            Room univerityCenter200 = new Room(30, "University Center", 200);
 
-        if (!univerityCenter200.getBuildingName().equals("University Center")) {
-            System.out.println("Something went wrong with getRoomNum");
-        } else {
-            System.out.println("getBuildingName works!");
+            if (univerityCenter200.getRoomNum() != 200) {
+                System.out.println("Something went wrong with getRoomNum");
+            } else {
+                System.out.println("getRoomNum works!");
+            }
+
+            if (univerityCenter200.getSeatingCap() != 30) {
+                System.out.println("Something went wrong with getRoomNum");
+            } else {
+                System.out.println("getSeatingCap works!");
+            }
+
+            if (!univerityCenter200.getBuildingName().equals("University Center")) {
+                System.out.println("Something went wrong with getRoomNum");
+            } else {
+                System.out.println("getBuildingName works!");
+            }
+        } catch (Exception e) {
+            System.out.println("Seating cap cannot be negative. Error: " + e);
         }
     }
 }
--- 2025-2026/CPSC-240/lab1/src/Room.java
+++ 2025-2026/CPSC-240/lab1/src/Room.java
@@ -1,40 +1,97 @@
+/**
+ * Describes a college classroom.
+ *
+ * @author Oscar Gaske
+ */
 public class Room {
+    /** The maximum number of people a given classroom can sit */
     private int seatingCap;
+
+    /** The name of the building where the class takes place */
     private String buildingName;
+
+    /** The number of the room */
     private int roomNum;
 
+    /* Default constructor */
     public Room() {
         this.seatingCap = -1;
         this.buildingName = "None";
         this.roomNum = -1;
     }
 
-    public Room(int seatingCap, String buildingName, int roomNum) {
+    /**
+     * Non-default constructor
+     *
+     * @param seatingCap the maximum number of people a given classroom can sit
+     * @param buildingName the name of the building where the class takes place
+     * @param roomNum the number of the room
+     * @throws IllegalArgumentException thrown if newSeatingCap is less than 0
+     */
+    public Room(int seatingCap, String buildingName, int roomNum) throws IllegalArgumentException {
+        if (seatingCap < 0) {
+            throw new IllegalArgumentException("Seating cap cannot be less than 0");
+        }
+
         this.seatingCap = seatingCap;
         this.buildingName = buildingName;
         this.roomNum = roomNum;
     }
 
+    /**
+     * Returns the current seating cap.
+     *
+     * @returns seatingCap the maximum number of people a given classroom can sit
+     */
     public int getSeatingCap() {
         return this.seatingCap;
     }
 
-    public void setSeatingCap(int newSeatingCap) {
+    /**
+     * Sets the seating cap. Throws an exception if the value to set is less than 0.
+     *
+     * @param newSeatingCap the new maximum number of people a given classroom can sit
+     * @throws IllegalArgumentException thrown if newSeatingCap is less than 0
+     */
+    public void setSeatingCap(int newSeatingCap) throws IllegalArgumentException {
+        if (newSeatingCap < 0) {
+            throw new IllegalArgumentException("Seating cap cannot be less than 0");
+        }
         this.seatingCap = newSeatingCap;
     }
 
+    /**
+     * Returns the current building name.
+     *
+     * @returns buildingName the current building name
+     */
     public String getBuildingName() {
         return this.buildingName;
     }
 
+    /**
+     * Sets the building name.
+     *
+     * @param newBuildingName the new building name
+     */
     public void setBuildingName(String newBuildingName) {
         this.buildingName = newBuildingName;
     }
 
+    /**
+     * Returns the room number.
+     *
+     * @return roomNum the current room number
+     */
     public int getRoomNum() {
         return this.roomNum;
     }
 
+    /**
+     * Sets the room number
+     *
+     * @param newRoomNum the new room number
+     */
     public void setRoomNum(int newRoomNum) {
         this.roomNum = newRoomNum;
     }
